73ca1f3b14718269ddbc2d67f244607acef85c86
liuduan <liuduan@fenbi.com>
2022-09-28 09:55:12 +0800

    MOD: update css

--- src/components/compound/compound.tsx
+++ src/components/compound/compound.tsx
@@ -1158,7 +1158,7 @@ export function Compound() {
                                 target="_blank"
                                 rel="external noreferrer"
                                 // href="https://twitter.com/compoundfinance"
-                                href="/api/twitter/oauth/v1"
+                                href="/api/twitter/oauth1"
                             ></a>
                         </div>
                     </div>
